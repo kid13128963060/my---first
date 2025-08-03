9743f91adc336a7ef6089a151fc5dde726ebb9df
kid1997 <kid1997@139.com>
2025-08-04 01:16:33 +0800

    增加关机功能

--- 试验夹/一键保存关机/一键保存关闭_txt_word_T1.py
+++ 试验夹/一键保存关机/一键保存关闭_txt_word_T1.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     # 执行关机操作
     try:
         # 设置关机倒计时（秒）
-        shutdown_delay = 80
+        shutdown_delay = 50
         print(f"将在{shutdown_delay}秒后关机...")
         print("若要取消关机，请打开命令提示符并输入：shutdown /a")
 
