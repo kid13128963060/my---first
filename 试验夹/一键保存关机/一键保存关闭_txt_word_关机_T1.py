37daca2b6169fad1b7deeacbd9d5375530dfa726
kid1997 <kid1997@139.com>
2025-08-12 10:50:41 +0800

    关机减少到5秒

--- 试验夹/一键保存关机/一键保存关闭_txt_word_关机_T1.py
+++ 试验夹/一键保存关机/一键保存关闭_txt_word_关机_T1.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
     # 执行关机操作
     try:
         # 设置关机倒计时（秒）
-        shutdown_delay = 30
+        shutdown_delay = 5
         print(f"将在{shutdown_delay}秒后关机...")
         print("若要取消关机，请打开命令提示符并输入：shutdown /a")
 
