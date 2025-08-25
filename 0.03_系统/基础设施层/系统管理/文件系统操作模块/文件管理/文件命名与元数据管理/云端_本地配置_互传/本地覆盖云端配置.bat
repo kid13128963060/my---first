03eb50d44e331754c8311245fee0ae1966c5ec10
kid1997 <kid1997@139.com>
2025-08-25 23:17:05 +0800

    本地覆盖云端bat修改

--- 0.03_系统/基础设施层/系统管理/文件系统操作模块/文件管理/文件命名与元数据管理/云端_本地配置_互传/本地覆盖云端配置.bat
+++ 0.03_系统/基础设施层/系统管理/文件系统操作模块/文件管理/文件命名与元数据管理/云端_本地配置_互传/本地覆盖云端配置.bat
@@ -4,7 +4,7 @@ set /p choice=请输入 [Y/N]:
 if /i "%choice%"=="Y" (
     echo 正在本地覆盖云端配置
 @echo off
-    powershell -WindowStyle Hidden -File "E:\备份盘\带零文件夹\005_计算机科学、程式、资料,硬件\005_400_电脑编程_1\文件系统操作模块\文件管理\文件命名与元数据管理\云端-本地配置-互传\005_411_PowerShell_本地覆盖云端配置.ps1"
+    powershell -WindowStyle Hidden -File "E:\备份盘_副\带零文件夹_副\005_计算机科学、程式、资料,硬件_副\005_400_电脑编程_1_副\Scripts_副\005_490_main\0.03_系统\基础设施层\系统管理\文件系统操作模块\文件管理\文件命名与元数据管理\云端_本地配置_互传\005_411_PowerShell_本地覆盖云端配置.ps1"
     echo 本地覆盖云端配置 ok
 ) else if /i "%choice%"=="N" (
     echo 操作已取消！
